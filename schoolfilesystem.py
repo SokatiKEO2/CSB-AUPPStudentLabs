ad41fd7819dd6b81c5fc775c5c4c3fbb7200e3c7
SokatiKEO2 <156150352+SokatiKEO2@users.noreply.github.com>
2024-01-15 11:25:42 +0700

    Update schoolfilesystem.py

--- schoolfilesystem.py
+++ schoolfilesystem.py
@@ -1,20 +1,38 @@
+import pandas
 
-#Libraries you may need:
-import csv, collections, dictionary, (pandas as pd), urlopen, etc..
 
-#classes and Functions to implement
 class SchoolAssessmentSystem:
-    def __init__():
+    
+    
+    def __init__(self):
+        self.data = pandas.DataFrame()
+    
+        
+    def process_file(self, file_path):   
+        try:            
+            with open(file_path, 'r') as file:
+                self.data = file.read()
+            return self.data
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+            return None
+        
+    
+    def transfer_data(self, old_file_path, new_file_path):
+        try:
+            with open(new_file_path, 'a') as file:
+                file.write(self.process_file(old_file_path))
+        except UnicodeDecodeError:
+            print("Error: Could not read file")
 
-    def process_file():            
+    # def fetch_web_data():
 
-    def transfer_data():
+    # def analyze_content():
 
-    def fetch_web_data():
-
-    def analyze_content():
-
-    def generate_summary():
+    # def generate_summary():
 
 
 # Analyze content & display result area
+bruh = SchoolAssessmentSystem()
+bruh.process_file("lol.xlsx")
+bruh.transfer_data(old_file_path="lol.xlsx",new_file_path="new.xlsx")
