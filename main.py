f39321ef500d497d2abee35c2de38ce00dfc5403
Sokati <2023475keo@aupp.edu.kh>
2024-01-17 10:34:12 +0700

    Last commit

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import pandas
 
 
 class_1 = sfs.SchoolAssessmentSystem()
-class_1.analyze_content("data/class_1.csv", "https://github.com/SokatiKEO2/CSB-AUPPStudentLabs/blob/main/Data/class_1_last_sem.csv?raw=true")
+class_1.analyze_content("data/class_1.csv", "https://raw.githubusercontent.com/SokatiKEO2/CSB-AUPPStudentLabs/main/data/class_1.csv")
 class_1.generate_summary("Class 1")
 
 class_2 = sfs.SchoolAssessmentSystem()
-class_2.analyze_content("data/class_2.csv", "https://github.com/SokatiKEO2/CSB-AUPPStudentLabs/blob/main/Data/class_2_last_sem.csv?raw=true")
+class_2.analyze_content("data/class_2.csv", "https://raw.githubusercontent.com/SokatiKEO2/CSB-AUPPStudentLabs/main/data/class_2.csv")
 class_2.generate_summary("Class 2")
 
 all_class = sfs.SchoolAssessmentSystem()
 all_class.read_file("data/class_1.csv")
 all_class.transfer_data("data/class_2.csv")
-all_class.analyze_content("data/merged_class.csv", "https://github.com/SokatiKEO2/CSB-AUPPStudentLabs/blob/main/Data/merged_class_last_sem.csv?raw=true")
+all_class.analyze_content("data/merged_class.csv", "https://raw.githubusercontent.com/SokatiKEO2/CSB-AUPPStudentLabs/main/data/merged_class.csv")
 all_class.generate_summary("Merged Class")
